e0ee8291a589cdfde31e215b4113215880ca97f3
mounirBen26 <mounir.bmh@hotmail.fr>
2024-10-25 23:12:53 +0100

    updated some changes

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,8 +1,10 @@
 exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Hello World"
-      })
-    }
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Allows requests from any origin
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify({ message: "Hello from Netlify!" }),
+  };
+};
